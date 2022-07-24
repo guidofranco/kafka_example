ceea5d7fd7465ca09ca7356a99c8fa5febb5dc89
Guido <gufranco@emeal.nttdata.com>
2022-07-24 10:58:27 -0300

    Logging stuff

--- download_static_data.py
+++ download_static_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 
+import logging
 import os
 from datetime import datetime
 
@@ -10,9 +11,13 @@ BASE_URL = "https://apitransporte.buenosaires.gob.ar"
 ENDPOINT = "ecobici/gbfs/stationInformation"
 FINAL_URL = f"{BASE_URL}/{ENDPOINT}?client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}"
 
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 response = requests.get(FINAL_URL)
 
 if response.status_code == 200:
+    logging.info(f"HTTP Status code: {response.status_code}")
     response = response.json()
 
     last_update = response["last_updated"]
@@ -20,13 +25,18 @@ if response.status_code == 200:
     update_date, update_time = last_update.strftime("%Y-%m-%d"), last_update.strftime("%H-%M")
     
     data_dir = f"data/stations/{update_date}/{update_time}"
+    logging.info(f"Creando directorio {data_dir}")
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
+    logging.info("Procesando datos")
     data = response["data"]["stations"]
     df_stations = pd.DataFrame(data)
     df_stations.to_csv(f"{data_dir}/stations.csv", index=None)
-
-    print("Success")
+    
+    logging.info(f"Los datos han sido almacenados en {data_dir}")
+    #print("Success")
 else:
-    print(response.status_code)
+    #print(response.status_code)
+    logging.error(f"Codigo de error HTTP: {response.status_code}")
+
